19c0470b0226cce682be41628f213a29898892f2
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-12-07 16:05:59 -0300

    Wines frontend

--- models/wine.rb
+++ models/wine.rb
@@ -1,3 +1,8 @@
 class Wine < ActiveRecord::Base
 	has_many :bottle_holders
+
+	validates :name, :presence=>true
+	validates :vintage, :presence=>true
+	validates :variety, :presence=>true
+	validates :volume, :presence=>true, :numericality=>true
 end
--- public/assets/js/wines_index.js
+++ public/assets/js/wines_index.js
@@ -1,3 +1,35 @@
 $(function(){
-	Newine.get_and_render_collection('wines');
+	//Newine.get_and_render_collection('wines');
+
+	$('.next_page').html("Siguiente >");
+	$('.previous_page').html("< Anterior");
+
+	var current_id = null;
+	$('#wines-container').on("click",".wine-link",function(){
+		var u_id = $(this).data('id');
+		if(current_id != u_id){
+			current_id = u_id;
+			$('.wine-data').hide();
+			$('.wine-data[data-wine-id=' + u_id+ ']').removeClass('hidden');
+			$('.wine-data[data-wine-id=' + u_id + ']').show('fade');
+		}
+	});
+
+	$('.wine-link:first').trigger('click');
+
+	
+	$('.edit_wine').click(function(){
+		$('.wine_info[data-wine-id=' + current_id + ']').hide();
+		$('.wine_edit_form[data-wine-id=' + current_id + ']').removeClass('hidden');
+		$(this).hide();
+	});
+	$('.cancel_edit').click(function(){
+		$('.wine_info[data-wine-id=' + current_id + ']').show();
+		$('.wine_edit_form[data-wine-id=' + current_id + ']').addClass('hidden');
+		$('.edit_wine[data-wine-id=' + current_id + ']').show();
+	})
+
+	$('#new_wine_btn').click(function(){
+		$('#new-wine-modal').modal('show');
+	});
 });
