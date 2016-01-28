aed94788f66f14037b8e61980b5e158b67584a36
Leonel Luchini <leonel.luchini@ec.gba.gov.ar>
2016-01-28 09:29:43 -0300

    agrego los valores por defecto en el bottle holder

--- models/bottle_holder.rb
+++ models/bottle_holder.rb
@@ -1,4 +1,13 @@
 class BottleHolder < ActiveRecord::Base
 	belongs_to :wine
 	belongs_to :dispenser
+
+	after_initialize :init
+
+  def init
+    self.serving_volume_low ||= wine.serving_volume_low 
+    self.serving_volume_med ||= wine.serving_volume_med
+    self.serving_volume_high ||= wine.serving_volume_high
+    self.serving_volume_micro ||= 25
+  end
 end
