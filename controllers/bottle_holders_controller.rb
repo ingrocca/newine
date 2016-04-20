ebc7abddb6f2059a95174ee926696bfcc4ea809b
Leonel Luchini <leo.luchi@gmail.com>
2016-04-20 15:11:59 -0300

    agrego la routa para rellenar la botella

--- controllers/bottle_holders_controller.rb
+++ controllers/bottle_holders_controller.rb
@@ -4,4 +4,10 @@ class NewineServer < Sinatra::Application
 		@bottle_holder.taste
 		{ status: 200, id: @bottle_holder.id }.to_json
 	end
+
+	post '/bottle_holder/:id/reload' do |id|
+		@bottle_holder = BottleHolder.find(id)
+		@bottle_holder.update(remaining_volume: @bottle_holder.wine.volume)
+		{ status: 200, id: @bottle_holder.id }.to_json
+	end
 end
