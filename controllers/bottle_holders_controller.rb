603fb9c7d7d91ad1e1200c9d7a1c660da90acc26
Leonel Luchini <leo.luchi@gmail.com>
2016-04-26 11:48:52 -0300

    cuando se recarga la botella o taste se manda la respuesta a la maquina

--- controllers/bottle_holders_controller.rb
+++ controllers/bottle_holders_controller.rb
@@ -3,6 +3,8 @@ class NewineServer < Sinatra::Application
 		data = JSON.parse(request.body.read)
 		@bottle_holder = BottleHolder.where(dispenser_id: data['dispenser_id'], dispenser_index: data['dispenser_index']).last
 		@bottle_holder.taste
+		@dispenser = @bottle_holder.dispenser
+		@dispenser.configure
 		{ status: 200, id: @bottle_holder.id }.to_json
 	end
 
@@ -10,7 +12,9 @@ class NewineServer < Sinatra::Application
 		data = JSON.parse(request.body.read)
 		@bottle_holder = BottleHolder.where(dispenser_id: data['dispenser_id'], dispenser_index: data['dispenser_index']).last
 		@bottle_holder.update(remaining_volume: @bottle_holder.wine.volume)
+		@dispenser = @bottle_holder.dispenser
+		@dispenser.configure
 		{ status: 200, id: @bottle_holder.id }.to_json
 	end
-	
+
 end
