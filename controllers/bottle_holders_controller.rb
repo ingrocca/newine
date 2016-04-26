efd133d6d239f2fb5a7a0583070b89e74df49c0c
Leonel Luchini <leo.luchi@gmail.com>
2016-04-26 12:48:25 -0300

    agrego el calculo de los dias para un bottle holder

--- controllers/bottle_holders_controller.rb
+++ controllers/bottle_holders_controller.rb
@@ -11,10 +11,16 @@ class NewineServer < Sinatra::Application
 	post '/bottle_holder/reload' do
 		data = JSON.parse(request.body.read)
 		@bottle_holder = BottleHolder.where(dispenser_id: data['dispenser_id'], dispenser_index: data['dispenser_index']).last
-		@bottle_holder.update(remaining_volume: @bottle_holder.wine.volume)
+		@bottle_holder.update(remaining_volume: @bottle_holder.wine.volume, date_bottle_change: Date.today)
 		@dispenser = @bottle_holder.dispenser
 		@dispenser.configure
 		{ status: 200, id: @bottle_holder.id }.to_json
+	end
+
+	post '/bottle_holder/days_bottle_change' do
+		data = JSON.parse(request.body.read)
+		@bottle_holder = BottleHolder.where(dispenser_id: data['dispenser_id'], dispenser_index: data['dispenser_index']).last
+		{ status: 200, days: @bottle_holder.open_days }.to_json
 	end
 
 end
