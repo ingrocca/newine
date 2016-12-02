f52eb5dee8de6eb226d739e686c19525dd23a362
Leonel Luchini <leo.luchi@gmail.com>
2016-12-01 21:35:40 -0300

    agrego los filtros al cambiar un vino

--- public/assets/js/dispensers_show.js
+++ public/assets/js/dispensers_show.js
@@ -254,4 +254,38 @@ $(function(){
 		
 	});
 
+	$("#searchInput").keyup(function () {
+    //split the current value of searchInput
+    var data = this.value.split(" ").filter(function(v){return v!==''});
+    //create a jquery object of the rows
+    var jo = $("#wines-container").find(".select-wine");
+    if (this.value == "") {
+        jo.show();
+        return;
+    }
+    //hide all the rows
+    jo.hide();
+
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this).text();
+
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.toLowerCase().indexOf(data[d]) >= 0){
+                return true;
+            }
+        }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+	}).focus(function () {
+    this.value = "";
+    $(this).css({
+        "color": "black"
+    });
+    $(this).unbind('focus');
+	}).css({
+    "color": "#C0C0C0"
+	});
 });
