9d3982effd41741242da57320b9481a882af1e2d
Leonel Luchini <leo.luchi@gmail.com>
2016-12-14 16:30:07 -0300

    subo total

--- controllers/tag_movements_controller.rb
+++ controllers/tag_movements_controller.rb
@@ -2,6 +2,7 @@
 class NewineServer < Sinatra::Application
 	get '/tag-movements' do
 		@q = TagMovement.ransack(params[:q])
+		@total = @q.result.sum(:credit)
 		@tag_movements = @q.result.order('created_at desc').paginate(:page=>params[:page], :per_page=>20)
 
 		format_render 'html', :"tag_movements/index"
