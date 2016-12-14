1773fb349613b7377239be6afaa9462fe5e72307
Leonel Luchini <leo.luchi@gmail.com>
2016-12-14 17:03:43 -0300

    subo por defecto los ultimos 30 días

--- controllers/tag_movements_controller.rb
+++ controllers/tag_movements_controller.rb
@@ -1,6 +1,10 @@
 #coding: utf-8
 class NewineServer < Sinatra::Application
 	get '/tag-movements' do
+		date_start = { created_at_gteq: Time.now - 30.days } unless params[:q]
+		date_end = { created_at_lteq: Time.now } unless params[:q]
+		params[:q] = date_start.merge(date_end) unless params[:q]
+		
 		@q = TagMovement.ransack(params[:q])
 		@total = @q.result.sum(:credit)
 		@tag_movements = @q.result.order('created_at desc').paginate(:page=>params[:page], :per_page=>20)
