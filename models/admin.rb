c84360a0a34e12902f36b69c63d4bec7593641a7
Leonel Luchini <leo.luchi@gmail.com>
2017-04-04 15:43:50 -0300

    fix validations admin

--- models/admin.rb
+++ models/admin.rb
@@ -1,10 +1,8 @@
 # Someone who uses this excellent piece of software.
 class Admin < ActiveRecord::Base
   include Shield::Model
-  
-  validates :username, :length => {:within => 3..40}
+
   validates :password, :length => {:within => 5..40}, on: :create
-  validates :username, presence: true, uniqueness: true, on: :create
   validates :email, presence: true, uniqueness: true, on: :create
   validates :password, presence: true, on: :create
   validates :password_confirmation, presence: true, on: :create
@@ -21,11 +19,6 @@ class Admin < ActiveRecord::Base
     def fetch(identifier)
       where(email: identifier).first || where(username: identifier).first
     end
-  end 
-
-  # Saves password (actually a hashed version of it)
-  def password=(pass)
-    self.crypted_password = Shield::Password.encrypt(pass)
   end
 
   # Mails a new password to someone who forgot it (not used, never tested, probably doesn't work)
