bca4ed55c2abe5ab66bcf3997cd759c62f0d6d16
Nicolas Pascual <nicolaspascual@hawkant.com>
2018-09-11 15:44:55 +0000

    adding docker run script

---
+++ newine_docker_start_black.sh
@@ -0,0 +1,2 @@
+docker container prune -f
+docker run -d --privileged --restart unless-stopped --mount source=newine-data-volume,target=/data -p 8080:8080 -p 3000:3000 -p 23:22 --name newine -t newine-black-server
