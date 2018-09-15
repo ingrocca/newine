a5a66e1bc0c9b5b446801edb15b9bbdf8dbe1156
Nicolas Pascual <nicolaspascual@hawkant.com>
2018-09-15 00:02:46 +0000

    updating startup scripts

--- newine_docker_start_black.sh
+++ newine_docker_start_black.sh
@@ -1,2 +1,2 @@
 docker container prune -f
-docker run -d --privileged --restart unless-stopped --mount source=newine-data-volume,target=/data -p 8080:8080 -p 3000:3000 -p 23:22 --name newine -t newine-black-server
+docker run -d --privileged --restart unless-stopped --mount source=newine-data-volume,target=/data -p 8080:8080 -p 3000:3000 -p 23:22 --name newine -t hawkant/newine-black-server
